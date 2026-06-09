60534ae1f2b968ada56fec65ec817e0b928f7df8
q15004040209-creator <q15004040209@gmail.com>
2026-06-09 20:18:25 +0800

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name="fastapi_wrap",
+    version="1.0.0",
+    packages=find_packages(),
+    description="现代高性能 Python Web 框架 - 自动交互式文档",
+    python_requires=">=3.8",
+)
